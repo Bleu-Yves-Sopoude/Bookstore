e20537b5069b568162bcfdb4e6eb39aec36ba979
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-10 16:09:58 +0000

    Add Api

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,48 +1,96 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// const appId = 9aaWCD6PToeD6wXGBluj
+const url =
+  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9aaWCD6PToeD6wXGBluj/books";
+export const getBooks = createAsyncThunk(
+  "books/fetchBooks",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios(url);
+      return response.data;
+    } catch (error) {
+      const errorMsg = `${error.code}: ${error.message}`;
+      return thunkAPI.rejectWithValue(errorMsg);
+    }
+  }
+);
+
+export const addBook = createAsyncThunk(
+  "books/addBook",
+  async (newBook, thunkAPI) => {
+    try {
+      const response = await axios.post(url, newBook);
+      thunkAPI.dispatch(getBooks());
+      return response.data;
+    } catch (error) {
+      const errorMsg = `${error.code}: ${error.message}`;
+      return thunkAPI.rejectWithValue(errorMsg);
+    }
+  }
+);
+
+export const removeBook = createAsyncThunk(
+  "books/removeBook",
+  async (endPoint, thunkAPI) => {
+    console.log(endPoint);
+    const removeURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9aaWCD6PToeD6wXGBluj/books/${endPoint}`;
+    try {
+      const response = await axios.delete(removeURL);
+      thunkAPI.dispatch(getBooks());
+      return response.data;
+    } catch (error) {
+      const errorMsg = `${error.code}: ${error.message}`;
+      return thunkAPI.rejectWithValue(errorMsg);
+    }
+  }
+);
 
 const initialState = {
-  bookArray: [
-    {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: 'item3',
-      title: 'Signature In The Cell',
-      author: 'Stephen C. Meyer',
-      category: 'Nonfiction',
-    },
-  ],
+  bookArray: [],
+  isLoading: false,
+  error: null,
+  added: null,
+  removed: null,
 };
 
 const bookSlice = createSlice({
-  name: 'bookList',
+  name: "bookList",
   initialState,
-  reducers: {
-    addBook: (store, { payload }) => {
-      const newBook = {
-        title: payload.title,
-        author: payload.author,
-        id: uuidv4(),
-      };
-      store.bookArray.push(newBook);
-    },
-    removeBook: (store, action) => {
-      const bookId = action.payload;
-      const newBookArray = store.bookArray.filter((book) => book.id !== bookId);
-      store.bookArray = newBookArray;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.bookArray = action.payload;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(addBook.pending, (state) => {
+        state.added = "sending...";
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.added = action.payload.data;
+      })
+      .addCase(addBook.rejected, (state, action) => {
+        state.added = action.payload.data;
+      })
+      .addCase(removeBook.pending, (state) => {
+        state.removed = "sending...";
+      })
+      .addCase(removeBook.fulfilled, (state, action) => {
+        state.removed = action.payload.data;
+      })
+      .addCase(removeBook.rejected, (state, action) => {
+        state.removed = action.payload.data;
+      });
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
